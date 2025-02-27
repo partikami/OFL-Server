3b4677e48a1ab46a106bc63ff99aaeae85e14665
partikami <km.partin@gmail.com>
2025-02-27 15:42:31 +0100

    fix: refactor hard coded connection string in db/connection.js to Heroku Var

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = process.env.ATLAS_URI || "";
-// const uri = "mongodb+srv://admin-michael:u1Bndij0sfWtBEvv@fields.ppvnlr8.mongodb.net/?retryWrites=true&w=majority&appName=records";
+const uri = process.env.MONGODB_URI || "";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
