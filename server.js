997cd27c2391776d0d8d1158ed899a10b88d424f
partikami <km.partin@gmail.com>
2025-02-27 20:57:06 +0100

    fix: added herokuapp.com to cors in server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://of-client-9c7ab85a5d42.herokuapp.com",
+  })
+);
 app.use(express.json());
 app.use("/record", records);
 
