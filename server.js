6c64c0a59cb28ec13a003d23bda932f1a4bb30b7
partikami <km.partin@gmail.com>
2025-02-27 22:30:38 +0100

    res.setHeader in server.js wieder gelöscht

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ app.use(
 app.use(express.json());
 app.use("/record", records);
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://of-client-9c7ab85a5d42.herokuapp.com"
-  );
-  // Other necessary headers
-  next();
-});
-
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
