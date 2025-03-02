4b11e228a4303e6cd1f8ff47acf027ccfe62a557
partikami <km.partin@gmail.com>
2025-03-02 14:07:10 +0100

    Local Beta

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = process.env.MONGODB_URI || "";
+// For deployment on heroku
+// const uri = process.env.MONGODB_URI || "";
+
+// For local development
+const uri = process.env.ATLAS_URI || "";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
--- server.js
+++ server.js
@@ -5,17 +5,24 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+// Local development
 app.use(
   cors({
-    origin: [
-      "https://of-client-9c7ab85a5d42.herokuapp.com",
-      "http://localhost:5173",
-    ],
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// Heroku deployment
+app.use(
+  cors({
+    origin: ["https://of-client-9c7ab85a5d42.herokuapp.com"],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use("/record", records);
 
